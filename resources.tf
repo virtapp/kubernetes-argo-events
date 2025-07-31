5758567715b18cb0518bd1effd71b219dcf4f916
Yevgeni Shapiro <23049337+virtapp@users.noreply.github.com>
2025-07-31 23:23:04 +0300

    update

---
+++ resources.tf
@@ -0,0 +1,19 @@
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "argo_events" {
+  name       = "argo-events"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-events"
+  namespace  = "argo-events"
+  version    = "2.6.3"  # Replace with the latest if needed
+  create_namespace = true
+
+  values = [
+    file("argo-events-values.yaml")  # optional, see below
+  ]
+}
