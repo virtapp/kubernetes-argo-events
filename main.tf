917a53dfe699fcca74d593a68945537d4b3ad765
Yevgeni Shapiro <23049337+virtapp@users.noreply.github.com>
2025-08-01 00:49:33 +0300

    update

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_providers {
     null = {
@@ -13,11 +12,25 @@ provider "null" {}
 resource "null_resource" "install_k3s" {
   provisioner "local-exec" {
     command = "export K3S_EXEC_FLAGS='--disable=traefik --disable=local-storage' && bash ./modules/k3s/install_k3s.sh ${var.node_role} ${var.k3s_version}"
-    #command = "bash ./modules/k3s/install_k3s.sh ${var.node_role} ${var.disable_components} ${var.k3s_version}"
   }
 
   triggers = {
     always_run = "${timestamp()}"
+
   }
 }
 
+resource "null_resource" "k3s_config" {
+  provisioner "local-exec" {
+    command = "mkdir -p ~/.kube/ && cp -r /etc/rancher/k3s/k3s.yaml ~/.kube/config"
+  }
+  depends_on = [null_resource.install_k3s]
+}
+
+
+resource "null_resource" "k3s_status" {
+  provisioner "local-exec" {
+    command = "sleep 5 && systemctl status k3s | grep -v k3s 2>/dev/null"
+  }
+  depends_on = [null_resource.k3s_config]
+}
