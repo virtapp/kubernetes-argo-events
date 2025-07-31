be160653644e684e4f7ae2aedb2e8de7dd3d0962
Yevgeni Shapiro <23049337+virtapp@users.noreply.github.com>
2025-08-01 00:36:01 +0300

    update

--- main.tf
+++ main.tf
@@ -12,7 +12,8 @@ provider "null" {}
 
 resource "null_resource" "install_k3s" {
   provisioner "local-exec" {
-    command = "bash ./modules/k3s/install_k3s.sh ${var.node_role} ${var.disable_components} ${var.k3s_version}"
+    command = "export K3S_EXEC_FLAGS='--disable=traefik --disable=local-storage' && bash ./modules/k3s/install_k3s.sh ${var.node_role} ${var.k3s_version}"
+    #command = "bash ./modules/k3s/install_k3s.sh ${var.node_role} ${var.disable_components} ${var.k3s_version}"
   }
 
   triggers = {
