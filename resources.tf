6e1394d22eb3eaa957f2cc1937831067d1de4134
Yevgeni Shapiro <23049337+virtapp@users.noreply.github.com>
2025-08-01 20:38:06 +0300

    update

--- resources.tf
+++ resources.tf
@@ -5,33 +5,13 @@ provider "helm" {
   }
 }
 
-resource "helm_release" "nginx" {
-  name       = "nginx"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "nginx"
-  namespace  = "ingress-nginx"
-  create_namespace = true
-  # Optional: override default chart values
-  values = [
-    <<EOF
-replicaCount: 2
-service:
-  type: LoadBalancer
-EOF
-  ]
-  depends_on = [null_resource.k3s_status]
+module "nginx" {
+  source = "./modules/nginx"
+  depends_on = [module.null_resource.k3s_status]
 }
 
-resource "helm_release" "argo_events" {
-  name       = "argo-events"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-events"
-  namespace  = "argo-events"
-  version    = "2.4.9"  # Replace with the latest if needed
-  create_namespace = true
-
-  #values = [
-  #  file("argo-events-values.yaml")  # optional, see below
-  #]
-  depends_on = [null_resource.k3s_status]
+module "argo-events" {
+  source = "./modules/argo-events"
+  depends_on = [module.null_resource.k3s_status]
 }
+
